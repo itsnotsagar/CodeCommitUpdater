0c69012cb9d1ba3912d6af3879cc6a08cb79358c
itsnotsagar <sagaryadav1029@gmail.com>
2024-01-05 17:18:16 +0530

    checkout main and run git pull for all repos

--- update-repos-script.ps1
+++ update-repos-script.ps1
@@ -18,3 +18,47 @@ foreach ($line in $lines) {
 # Print all profile names
 Write-Output "Profiles found:"
 $profiles
+
+# Ask the user if they want to update the SSO profile
+$updateSsoProfile = Read-Host -Prompt "Do you want to update the SSO profile? (yes/no)"
+
+if ($updateSsoProfile -eq "yes") {
+    # Ask the user for which profile they want to login to AWS SSO
+    $selectedProfile = Read-Host -Prompt "Enter the profile you want to login to AWS SSO with (leave empty to update all profiles)"
+
+    if ($selectedProfile) {
+        if ($profiles -contains $selectedProfile) {
+            # Run aws sso login command for the selected profile
+            Write-Output "Logging in to AWS SSO with profile $selectedProfile"
+            Start-Process -Wait -NoNewWindow -FilePath "aws" -ArgumentList "sso login --profile $selectedProfile"
+        } else {
+            Write-Output "Profile $selectedProfile not found"
+        }
+    } else {
+        # Run aws sso login command for all profiles
+        $profiles | ForEach-Object {
+            Write-Output "Logging in to AWS SSO with profile $_"
+            Start-Process -Wait -NoNewWindow -FilePath "aws" -ArgumentList "sso login --profile $_"
+        }
+    }
+}
+# Specify the directory containing the repos
+$dir = "C:\Users\test\REPO"
+
+# Get all subdirectories
+$directories = Get-ChildItem -Path $dir -Directory
+
+# Loop over all subdirectories in parallel
+$directories | ForEach-Object -Parallel {
+    # Change to the directory
+    Set-Location -Path $_.FullName
+    
+    # Check if it's a git repository
+    if (Test-Path .git) {
+        Write-Output "Updating $($_.FullName)"
+        
+        # Checkout main branch and pull
+        git checkout main
+        git pull
+    }
+}
